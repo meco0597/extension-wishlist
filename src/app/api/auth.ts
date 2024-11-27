8dec1fb8c1208f2fa0b68761ae06764c385b11c2
Melvin <meco0597@gmail.com>
2024-11-26 21:28:10 -0700

    fix

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -7,7 +7,7 @@ import config from '@/config';
 
 const adminConfig: ServiceAccount = {
     projectId: process.env.FIREBASE_PROJECT_ID!,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')!,
+    privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
 };
 
